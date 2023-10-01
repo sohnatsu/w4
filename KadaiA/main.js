eeddab1abfa6f02e21b234dec1203147ae31ea3b
Càiyuè <109915805+sohnatsu@users.noreply.github.com>
2023-10-01 21:46:43 +0900

    Update main.js

--- KadaiA/main.js
+++ KadaiA/main.js
@@ -7,7 +7,8 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-const storyText = "津田塾大学は華氏94度だったので :insertx: は散歩に出かけた。 :inserty: の所に着くと、二人はしばらく恐怖の目で見つめ、それから :insertz: 。 ボブはその一部始終を見たが、 :insertx: の体重が300ポンドもあることには驚かなかった。 :inserty: ではよく起こることであったのだ。";
+const storyText = "津田塾大学は華氏94度だったので :insertx: は散歩に出かけた。 :insertx: 。 :inserty: の所に着くと、二人はしばらく恐怖の目で見つめ、それから :insertz: 。 ボブはその一部始終を見たが、 :insertx: の体重が300ポンドもあることには驚かなかった。 :inserty: ではよく起こることであったのだ。";
+const insertW = ["7色の虹が空にかかっていた", "雷が鳴り響いていた", "大きな月が空からのぞいていた"];
 const insertX = ["犬夜叉", "飛影", "怪盗キッド"];
 const insertY = ["千葉ニュータウン", "箱根温泉", "富士急ハイランド"];
 const insertZ = ["何かが爆発した", "ナメクジになって這って逃げた", "ムーンウォークでどこかへ向かった"];
@@ -16,11 +17,13 @@ randomize.addEventListener('click', result);
 
 function result() {
   let newStory = storyText;
-
+  
+  const wItem = randomValueFromArray(insertW);
   const xItem = randomValueFromArray(insertX);
   const yItem = randomValueFromArray(insertY);
   const zItem = randomValueFromArray(insertZ);
-
+  
+  newStory = newStory.replace(':insertw:',wItem);
   newStory = newStory.replace(':insertx:',xItem);
   newStory = newStory.replace(':insertx:',xItem);
   newStory = newStory.replace(':inserty:',yItem);
